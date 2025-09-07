ab32c19173f7cc324ca5d6ec9a8036ee6b650089
Muhammad Mansoor <mansoorm@bemobi.com>
2025-09-07 20:10:23 +0500

    Fix models and controllers

--- config/paddle.php
+++ config/paddle.php
@@ -1,20 +1,13 @@
 <?php
 
 return [
-
-    'vendor_id' => env('PADDLE_VENDOR_ID'),
-    'api_key' => env('PADDLE_API_KEY'),
-
-    // Put the public key directly here (multi-line supported in PHP arrays)
-    'public_key' => <<<EOD
------BEGIN PUBLIC KEY-----
-MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA...
-...rest of your Paddle key...
------END PUBLIC KEY-----
-EOD,
-
-    'webhook_secret' => env('PADDLE_WEBHOOK_SECRET'),
-    'environment' => env('PADDLE_ENVIRONMENT', 'sandbox'),
-    'success_url' => env('PADDLE_SUCCESS_URL'),
-    'cancel_url' => env('PADDLE_CANCEL_URL'),
+    'paddle' => [
+        'vendor_id' => env('PADDLE_VENDOR_ID'),
+        'api_key' => env('PADDLE_API_KEY'),
+        'public_key' => '-----BEGIN PUBLIC KEY-----MIIBIjAN...',
+        'webhook_secret' => env('PADDLE_WEBHOOK_SECRET'),
+        'environment' => env('PADDLE_ENVIRONMENT', 'sandbox'),
+        'success_url' => env('PADDLE_SUCCESS_URL'),
+        'cancel_url' => env('PADDLE_CANCEL_URL'),
+    ],
 ];

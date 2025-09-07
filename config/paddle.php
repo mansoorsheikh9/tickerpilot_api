1d6203333041f969012278e76395fc0c194e577a
Muhammad Mansoor <mansoorm@bemobi.com>
2025-09-07 20:03:46 +0500

    Fix models and controllers

--- config/paddle.php
+++ config/paddle.php
@@ -2,25 +2,19 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
-    */
+    'vendor_id' => env('PADDLE_VENDOR_ID'),
+    'api_key' => env('PADDLE_API_KEY'),
 
-    'paddle' => [
-        'vendor_id' => env('PADDLE_VENDOR_ID'),
-        'api_key' => env('PADDLE_API_KEY'),
-        'public_key' => env('PADDLE_PUBLIC_KEY'),
-        'webhook_secret' => env('PADDLE_WEBHOOK_SECRET'),
-        'environment' => env('PADDLE_ENVIRONMENT', 'sandbox'),
-        'success_url' => env('PADDLE_SUCCESS_URL'),
-        'cancel_url' => env('PADDLE_CANCEL_URL'),
-    ],
+    // Put the public key directly here (multi-line supported in PHP arrays)
+    'public_key' => <<<EOD
+-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA...
+...rest of your Paddle key...
+-----END PUBLIC KEY-----
+EOD,
+
+    'webhook_secret' => env('PADDLE_WEBHOOK_SECRET'),
+    'environment' => env('PADDLE_ENVIRONMENT', 'sandbox'),
+    'success_url' => env('PADDLE_SUCCESS_URL'),
+    'cancel_url' => env('PADDLE_CANCEL_URL'),
 ];

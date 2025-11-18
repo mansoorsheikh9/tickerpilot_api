d85b14fc857a49f9a34ea0f263a9eb13b97bf485
Muhammad Mansoor <mansoorm@bemobi.com>
2025-11-18 13:07:57 +0500

    Forget/Reset password function

---
+++ resources/views/emails/password-reset-text.blade.php
@@ -0,0 +1,17 @@
+Hello {{ $user->name }},
+
+We received a request to reset your password for your TickerPilot account.
+
+To reset your password, please visit the following link:
+{{ $resetUrl }}
+
+This link will expire in 60 minutes.
+
+If you didn't request this password reset, please ignore this email or contact our support team.
+
+---
+Best regards,
+The TickerPilot Team
+
+© {{ date('Y') }} TickerPilot. All rights reserved.
+Need help? Contact us at support@tickerpilot.com
